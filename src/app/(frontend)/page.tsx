a803d79a2c0e4c237810a665bf216662f106b1b9
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-06-16 14:10:56 +1200

    contact component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,6 +5,7 @@ import AboutUs from "@/components/home/AboutUs";
 import FollettoFeatures from "@/components/home/FollettoFeatures";
 import Menu from "@/components/home/Menu";
 import Franchisee from "@/components/home/Franchisee";
+import Contact from "@/components/home/Contact";
 
 export default function Home() {
   return (
@@ -13,16 +14,23 @@ export default function Home() {
           <div className="responsive-fullwidth mt-75 sm:mt-55 bg-background-black py-10 px-22">
               <InfoBanner/>
           </div>
+
           <AboutUs />
+
           <div className="responsive-fullwidth my-32">
               <FollettoFeatures />
           </div>
+
           <div className="responsive-fullwidth mb-48 mx-auto">
               <Menu />
           </div>
 
-          <div className="responsive-fullwidth mb-32">
+          <div className="responsive-fullwidth mb-20">
               <Franchisee />
+          </div>
+
+          <div className="mb-20">
+              <Contact />
           </div>
       </StandardLayout>
 );
---
+++ src/components/home/Contact.tsx
@@ -0,0 +1,22 @@
+import Title from "@/components/ui/Title";
+import Link from "next/link";
+import Button from "@/components/ui/Button";
+
+export default function Contact() {
+    return (
+        <div className="flex flex-col justify-center items-center gap-y-7">
+            <Title title={"Write Us a Message"} />
+            <p className="text-center">
+                Have any questions? Get in contact with us using the link below!
+                <p>
+                    Our team will be in touch with you within 48-hours.
+                </p>
+            </p>
+            <Button className="text-primary-brown">
+                <Link href="/contact">
+                    Contact Us
+                </Link>
+            </Button>
+        </div>
+    );
+}
